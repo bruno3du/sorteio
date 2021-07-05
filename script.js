9f19cd27870ead25c693a3cea955d63bcea6538f
Bruno Eduardo <bruno.3duardo@hotmail.com>
2021-07-05 12:44:48 -0300

    efeitos e resposividade

--- script.js
+++ script.js
@@ -1,17 +1,44 @@
-const campo1 = document.getElementById("campo1")
-const campo2 = document.getElementById("campo2")
-const botao = document.getElementById("botao")
+let campo1 = document.getElementById("campo1")
+let campo2 = document.getElementById("campo2")
+let botao = document.getElementsByClassName("btn")[0]
+const numberSorte = document.getElementsByClassName("numSorte")[0]
 
 function random(min, max) {
     return Math.floor(Math.random() * (max - min + 1) ) + min
 
 }
 
-
 botao.addEventListener ('click', () => {
     const sort = document.getElementById('sorteado')
     const min = Number(campo1.value)
     const max = Number(campo2.value)
     const resultado = random(min, max)
-    sort.innerHTML = resultado
+    
+        if(numberSorte.classList[1] != "ns-1") {
+            setTimeout(()=> {sort.innerHTML = resultado}, 1600)
+        } else {
+            sort.innerHTML = resultado
+        }
+
+    numberSorte.classList.add("ns-1")
+    setTimeout(()=> {numberSorte.innerHTML = "<p>Seu número da sorte é:</p>"}, 500) 
+    console.log(numberSorte.classList[1])
+})
+
+
+// Efeito no botão
+
+botao.addEventListener("click", function(e) {
+let x = e.layerX; 
+let y = e.layerY;
+    
+
+    let ripples = document.createElement('span');
+    ripples.style.left = x + 'px';
+    ripples.style.top = y + 'px';
+    this.appendChild(ripples);
+
+    setTimeout(function() {
+        ripples.remove()
+    },900)
 })
